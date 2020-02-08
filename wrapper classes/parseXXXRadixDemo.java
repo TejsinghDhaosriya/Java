3b586283bb7724d06d4b523c1040b9cea9dba8dd
TejsinghDhaosirya <tejsinghdhaosriya@gmail.com>
2020-02-09 02:00:31 +0530

    parseXXX with radix

---
+++ wrapper classes/parseXXXRadixDemo.java
@@ -0,0 +1,16 @@
+public class parseXXXRadixDemo
+     {
+	 public static void main(String[] args)
+	    {
+	     int i = Integer.parseInt("1011111111111",2);
+      //   boolean d= Boolean.parseBoolean("10.5",2);
+        // double d2= Double.parseDouble("10.5",2);
+         //float f=Float.parseFloat("10.4",2	);
+         System.out.println(i);		 
+  		
+		}
+	 
+	 
+	 }
+	 
+	 
