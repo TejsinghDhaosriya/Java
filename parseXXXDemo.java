217a67b6807e5384a9a6f6bab3db16cdf6d31cb4
TejsinghDhaosirya <tejsinghdhaosriya@gmail.com>
2020-02-09 01:56:00 +0530

    parseXXX

---
+++ parseXXXDemo.java
@@ -0,0 +1,16 @@
+public class parseXXXDemo
+     {
+	 public static void main(String[] args)
+	    {
+	     int i = Integer.parseInt("10.5");
+         boolean d= Boolean.parseBoolean("10.5");
+         double d2= Double.parseDouble("10.5");
+         float f=Float.parseFloat("10.4");
+         System.out.println(i+"/n"+d+"/n"+d2+"/n"+f);		 
+  		
+		}
+	 
+	 
+	 }
+	 
+	 
