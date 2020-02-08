7bc0e14e2100f274787da58b66f9f539f6667f11
TejsinghDhaosirya <tejsinghdhaosriya@gmail.com>
2020-02-09 02:04:55 +0530

    toString method of wrapper class

---
+++ wrapper classes/toStringDemo.java
@@ -0,0 +1,10 @@
+public class toStringDemo
+     {
+	 public static void main(String[] args)
+	     {
+		 Integer i = new Integer(10);
+		 String s = i.toString();
+         System.out.println(s);		 
+		 }
+	 
+	 }
